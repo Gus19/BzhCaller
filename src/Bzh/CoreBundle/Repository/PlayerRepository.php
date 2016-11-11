90f81a78570763445ef0be9e882f90596a3ea1a0
Gus19 <Julien@Gus.local>
2016-11-11 20:47:15 +0100

    utilisation de la table player

--- src/Bzh/CoreBundle/Repository/PlayerRepository.php
+++ src/Bzh/CoreBundle/Repository/PlayerRepository.php
@@ -13,6 +13,8 @@ class PlayerRepository extends \Doctrine\ORM\EntityRepository
     public function setOldAllPlayers(\Bzh\CoreBundle\Entity\Clan $clan, $value) {
         return $this->createQueryBuilder('u')
             ->update()
+            ->where('u.clan = :clan')
+                ->setParameter(":clan", $clan)
             ->set('u.old', ':old')
                 ->setParameter(":old", $value)
             ->getQuery()->getSingleScalarResult();
